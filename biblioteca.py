20468c49cc37ac09950c8427c6e79a6141e23d3e
chris <christopherrequenesantana@gmail.com>
2026-06-21 21:10:06 -0500

    Update biblioteca.py

--- biblioteca.py
+++ biblioteca.py
@@ -1,4 +1,4 @@
-# Sistema de Gestión de Biblioteca
+
 
 libros = []
 
@@ -15,20 +15,50 @@ def mostrar_libros():
         for libro in libros:
             print("-", libro)
 
+def buscar_libro():
+    nombre = input("Ingrese el libro a buscar: ")
+
+    if nombre in libros:
+        print("Libro encontrado.")
+    else:
+        print("Libro no encontrado.")
+
+def eliminar_libro():
+    nombre = input("Ingrese el libro a eliminar: ")
+
+    if nombre in libros:
+        libros.remove(nombre)
+        print("Libro eliminado.")
+    else:
+        print("Libro no existe.")
+
+# Menú principal
 while True:
+
     print("\n===== SISTEMA DE BIBLIOTECA =====")
     print("1. Registrar libro")
     print("2. Mostrar libros")
-    print("3. Salir")
+    print("3. Buscar libro")
+    print("4. Eliminar libro")
+    print("5. Salir")
 
     opcion = input("Seleccione una opción: ")
 
     if opcion == "1":
         registrar_libro()
+
     elif opcion == "2":
         mostrar_libros()
+
     elif opcion == "3":
+        buscar_libro()
+
+    elif opcion == "4":
+        eliminar_libro()
+
+    elif opcion == "5":
         print("Programa finalizado.")
         break
+
     else:
         print("Opción inválida.")
